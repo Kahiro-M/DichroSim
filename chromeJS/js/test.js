f57a6f42b6e58a7fed33e7a3a16fbd874279047f
KahiroMatsudaira <ci013069@gmail.com>
2018-03-11 18:50:09 +0900

    JSにmain関数を追加

--- chromeJS/js/test.js
+++ chromeJS/js/test.js
@@ -1,7 +1,11 @@
+function main(){
+  test();
+}
 function test(){
   var count = 1;
   count = count + 2;
   count = count + 3;
   count = count + 4;
+  count = count + 5;
   alert('演算結果は  ' + count);
 }
