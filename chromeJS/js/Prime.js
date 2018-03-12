6953fb0cb0b10f561b0a021d45899231e753b300
RKC/038173(松平圭央) <k_matsu@riso.co.jp>
2018-03-12 10:37:19 +0900

    範囲内の素数を列挙する関数を追加

--- chromeJS/js/Prime.js
+++ chromeJS/js/Prime.js
@@ -1,11 +1,45 @@
 //引数が素数だったら1(true),素数ではなかったら0(false)を返す
 function isPrime(iInput) {
-	for(let i=2;i<iInput;i++){
-		let ret = iInput%i;
-		if(ret==0){
-			return ret;
+	if(iInput<2){
+			return 0;
+		}
+	else{
+		for(let i=2;i<iInput;i++){
+			let ret = iInput%i;
+			if(ret==0){
+				return 0;
+			}
+		}
+		return 1;
+	}
+}
+
+//Start～Endまでの間の素数を格納した配列を返す
+function getPrimeArray(iInStart,iInEnd){
+	let ret = [];
+	let tmp = 0;
+	
+	//先頭と終端の関係チェック
+	if(iInStart<iInEnd){
+		//問題なし
+	}
+	else{
+		ret.push('引数の関係にエラー有');
+		return ret;
+	}
+	
+	//先頭から終端までの素数を配列に格納
+	for(let i=iInStart; i<=iInEnd; i++){
+		tmp = isPrime(i);
+		if(tmp){
+			ret.push(i);
 		}
 	}
-	ret = 1;
+	
+	//素数がなかった場合、配列にコメントを入れる
+	if(ret.length == 0){
+		ret.push('無かったよ');
+	}
+	
 	return ret;
 }
--- chromeJS/js/test.js
+++ chromeJS/js/test.js
@@ -1,14 +1,56 @@
 function main() {
 	"use strict";
-	var iTest = isPrime(4);
-	alert('結果は'+iTest)
+	//alert('inputForm.Start.value:'+ document.inputForm.Start.value);
+	//alert('inputForm.End.value:'+ document.inputForm.End.value);
+	
+	//htmlから引数を取得
+	var iInStart = document.inputForm.Start.value;
+	var iInEnd = document.inputForm.End.value;
+	var aPrime = [];
+
+	//Start～Endまでの間の素数を列挙する
+	/*
+	let ret = 0;
+	for(let i=iInStart; i<iInEnd; i++){
+		ret = isPrime(i);
+		if(ret){
+			alert(i+'は素数');
+		}
+	}
+	*/
+	aPrime = getPrimeArray(iInStart,iInEnd);
+	console.log('素数は'+aPrime);
+	alert(aPrime);
+
 }
 
-	function test(){
-  var count = 1;
-  count = count + 2;
-  count = count + 3;
-  count = count + 4;
-  count = count + 5;
-  alert('演算結果は  ' + count);
-}
+//function getPrimeArray(iInStart,iInEnd){
+//	let ret = [];
+//	let tmp = 0;
+//	
+//	//先頭と終端の関係チェック
+//	if(iInStart<iInEnd){
+//		//問題なし
+//	}
+//	else{
+//		ret.push('引数の関係にエラー有');
+//		return ret;
+//	}
+//	
+//	//先頭から終端までの素数を配列に格納
+//	for(let i=iInStart; i<=iInEnd; i++){
+//		tmp = isPrime(i);
+//		if(tmp){
+//			//console.log(i+'は素数');
+//			ret.push(i);
+//		}
+//	}
+//	//console.log(ret);
+//	
+//	//素数がなかった場合、配列にコメントを入れる
+//	if(ret.length == 0){
+//		ret.push('無かったよ');
+//	}
+//	
+//	return ret;
+//}
