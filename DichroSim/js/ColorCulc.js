e2486a7c089f586d497cd39370cb9f4521e8a7b4
RKC/038173(松平圭央) <k_matsu@riso.co.jp>
2018-07-17 16:52:07 +0900

    参考文献追加

--- DichroSim/js/ColorCulc.js
+++ DichroSim/js/ColorCulc.js
@@ -5,6 +5,7 @@ function ColorData(x,y,Y){
 	this.Y = Number(Y);
 	this.X = this.x/this.y*this.Y;
 	this.Z = this.z/this.y*this.Y;
+	return	this;
 }
 
 function addColor(Color1, Color2){
@@ -15,5 +16,16 @@ function addColor(Color1, Color2){
 	tmpColor.x = tmpColor.X / (tmpColor.X+tmpColor.Y+tmpColor.Z);
 	tmpColor.y = tmpColor.Y / (tmpColor.X+tmpColor.Y+tmpColor.Z);
 	tmpColor.z = tmpColor.Z / (tmpColor.X+tmpColor.Y+tmpColor.Z);
+	return tmpColor;
+}
+
+function subColor(Color1, Color2){
+	var tmpColor = new ColorData();
+	tmpColor.X = Color1.X-Color2.X;
+	tmpColor.Y = Color1.Y-Color2.Y;
+	tmpColor.Z = Color1.Z-Color2.Z;
+	tmpColor.x = tmpColor.X / (tmpColor.X-tmpColor.Y-tmpColor.Z);
+	tmpColor.y = tmpColor.Y / (tmpColor.X-tmpColor.Y-tmpColor.Z);
+	tmpColor.z = tmpColor.Z / (tmpColor.X-tmpColor.Y-tmpColor.Z);
 	return tmpColor;
 }
--- DichroSim/js/main.js
+++ DichroSim/js/main.js
@@ -1,9 +1,5 @@
 function main() {
 	"use strict";
-	//alert('formInput.Start.value:'+ document.formInput.Start.value);
-	//alert('formInput.End.value:'+ document.formInput.End.value);
-
-
 
 	//htmlから引数を取得
 	var Color1 = new ColorData(	document.formInput.Color1_x.value,
@@ -13,15 +9,11 @@ function main() {
 								 	document.formInput.Color2_x.value,
 								 	document.formInput.Color2_Y.value	);
 
-
 	var SumColor = addColor(Color1,Color2);
-
-	var aPrime = [];
-
-	//Start～Endまでの間の素数を列挙する
-
-	aPrime = getPrimeArray(iInStart,iInEnd);
-	console.log('素数は'+aPrime);
-	alert(aPrime);
+	var SubColor = subColor(Color1,Color2);
+	
+	console.log(SumColor);
+	console.log(SubColor);
+//	alert(SumColor);
 
 }
