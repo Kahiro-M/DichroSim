d0270fbb78e8cd29d78043f6975d129a439e70ad
RKC/038173(松平圭央) <k_matsu@riso.co.jp>
2018-07-22 12:29:42 +0900

    LMS変換処理追加

--- DichroSim/js/ColorCulc.js
+++ DichroSim/js/ColorCulc.js
@@ -8,6 +8,7 @@ function ColorData(){
 	return	this;
 }
 
+
 function culcXYZ(x,y,Y){
 	this.x = Number(x);
 	this.y = Number(y);
@@ -18,12 +19,17 @@ function culcXYZ(x,y,Y){
 	return	this;
 }
 
-function addColor(Color1, Color2){
+function makeXYZMatrix(inColorData){
+	var tmp = math.matrix([inColorData.X,inColorData.Y,inColorData.Z]);
+	return	tmp;
+}
+
+function addColor(inColor1, inColor2){
 	var tmpColor = new ColorData();
 	var sumXYZ;
-	tmpColor.X = Color1.X+Color2.X;
-	tmpColor.Y = Color1.Y+Color2.Y;
-	tmpColor.Z = Color1.Z+Color2.Z;
+	tmpColor.X = inColor1.X+inColor2.X;
+	tmpColor.Y = inColor1.Y+inColor2.Y;
+	tmpColor.Z = inColor1.Z+inColor2.Z;
 	sumXYZ = tmpColor.X+tmpColor.Y+tmpColor.Z;
 	tmpColor.x = tmpColor.X / sumXYZ;
 	tmpColor.y = tmpColor.Y / sumXYZ;
@@ -32,13 +38,35 @@ function addColor(Color1, Color2){
 	return tmpColor;
 }
 
-function subColor(Color1, Color2){
+function subColor(inColor1, inColor2){
 	var tmpColor = new ColorData();
-	tmpColor.X = Color1.X-Color2.X;
-	tmpColor.Y = Color1.Y-Color2.Y;
-	tmpColor.Z = Color1.Z-Color2.Z;
+	tmpColor.X = inColor1.X-inColor2.X;
+	tmpColor.Y = inColor1.Y-inColor2.Y;
+	tmpColor.Z = inColor1.Z-inColor2.Z;
 	tmpColor.x = tmpColor.X / (tmpColor.X-tmpColor.Y-tmpColor.Z);
 	tmpColor.y = tmpColor.Y / (tmpColor.X-tmpColor.Y-tmpColor.Z);
 	tmpColor.z = tmpColor.Z / (tmpColor.X-tmpColor.Y-tmpColor.Z);
 	return tmpColor;
+}
+
+function transLMS(inColor){
+	//LMS錐体の反応値への変換行列
+	//Vos(1978)の錐体分好感度を採用
+	var Vos = VosMatrix();
+//		math.matrix([	[0.15516, 0.54307, -0.03701],
+//							[-0.15516, 0.45692, 0.02969],
+//							[0, 0, 0.00732]]);
+
+	var mColor = makeXYZMatrix(inColor);
+	var LMS = math.multiply(Vos,mColor);
+	return LMS;
+}
+
+function VosMatrix(){
+	//LMS錐体の反応値への変換行列
+	//Vos(1978)の錐体分好感度を採用
+	return math.matrix([	[0.15516, 0.54307, -0.03701],
+							[-0.15516, 0.45692, 0.02969],
+							[0, 0, 0.00732]]);
+
 }
--- DichroSim/js/main.js
+++ DichroSim/js/main.js
@@ -2,18 +2,30 @@ function main() {
 	"use strict";
 
 	//htmlから引数を取得
-	var Color1 = new culcXYZ(	document.formInput.Color1_x.value,
-								document.formInput.Color1_y.value,
-								document.formInput.Color1_Y.value	);
-	var Color2 = new culcXYZ(	document.formInput.Color2_x.value,
-								document.formInput.Color2_y.value,
-								document.formInput.Color2_Y.value	);
+	var Color1 = new ColorData();
+	Color1 = culcXYZ(	document.formInput.Color1_x.value,
+						document.formInput.Color1_y.value,
+						document.formInput.Color1_Y.value	);
+	
+	var Color2 = new ColorData();
+	Color2 = culcXYZ(	document.formInput.Color2_x.value,
+						document.formInput.Color2_y.value,
+						document.formInput.Color2_Y.value	);
 
+	var mColor1 = makeXYZMatrix(Color1);
+	var mColor2 = makeXYZMatrix(Color2);
+	
+	var lmsColor1 = transLMS(Color1);
+	
 	var SumColor = addColor(Color1,Color2);
 	var SubColor = subColor(Color1,Color2);
 	
+	console.log(Color1);
+	console.log(Color2);
 	console.log(SumColor);
 	console.log(SubColor);
+	console.log(mColor1);
+	console.log(mColor2);
 	
 	console.log(math.round(math.e, 3));            // 2.718
 
