981988eb0b2494bbdf7eadd41ea6f69f51d47bc1
RKC/038173(松平圭央) <k_matsu@riso.co.jp>
2018-07-22 11:28:42 +0900

    色情報構造体の扱いを追加

--- DichroSim/js/ColorCulc.js
+++ DichroSim/js/ColorCulc.js
@@ -1,21 +1,34 @@
-function ColorData(x,y,Y){
+function ColorData(){
+	this.x = 0;
+	this.y = 0;
+	this.z = 0;
+	this.Y = 0;
+	this.X = 0;
+	this.Z = 0;
+	return	this;
+}
+
+function culcXYZ(x,y,Y){
 	this.x = Number(x);
 	this.y = Number(y);
-	this.z = 1-this.x-this.y;
+	this.z = 1-(this.x+this.y);
 	this.Y = Number(Y);
-	this.X = this.x/this.y*this.Y;
-	this.Z = this.z/this.y*this.Y;
+	this.X = (this.x/this.y)*this.Y;
+	this.Z = (this.z/this.y)*this.Y;
 	return	this;
 }
 
 function addColor(Color1, Color2){
 	var tmpColor = new ColorData();
+	var sumXYZ;
 	tmpColor.X = Color1.X+Color2.X;
 	tmpColor.Y = Color1.Y+Color2.Y;
 	tmpColor.Z = Color1.Z+Color2.Z;
-	tmpColor.x = tmpColor.X / (tmpColor.X+tmpColor.Y+tmpColor.Z);
-	tmpColor.y = tmpColor.Y / (tmpColor.X+tmpColor.Y+tmpColor.Z);
-	tmpColor.z = tmpColor.Z / (tmpColor.X+tmpColor.Y+tmpColor.Z);
+	sumXYZ = tmpColor.X+tmpColor.Y+tmpColor.Z;
+	tmpColor.x = tmpColor.X / sumXYZ;
+	tmpColor.y = tmpColor.Y / sumXYZ;
+	tmpColor.z = tmpColor.Z / sumXYZ;
+	
 	return tmpColor;
 }
 
--- DichroSim/js/main.js
+++ DichroSim/js/main.js
@@ -2,12 +2,12 @@ function main() {
 	"use strict";
 
 	//htmlから引数を取得
-	var Color1 = new ColorData(	document.formInput.Color1_x.value,
-								 	document.formInput.Color1_x.value,
-								 	document.formInput.Color1_Y.value	);
-	var Color2 = new ColorData(	document.formInput.Color2_x.value,
-								 	document.formInput.Color2_x.value,
-								 	document.formInput.Color2_Y.value	);
+	var Color1 = new culcXYZ(	document.formInput.Color1_x.value,
+								document.formInput.Color1_y.value,
+								document.formInput.Color1_Y.value	);
+	var Color2 = new culcXYZ(	document.formInput.Color2_x.value,
+								document.formInput.Color2_y.value,
+								document.formInput.Color2_Y.value	);
 
 	var SumColor = addColor(Color1,Color2);
 	var SubColor = subColor(Color1,Color2);
