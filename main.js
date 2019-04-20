0a452cb7a902f91affa644c8f08a42f3c162b9cb
kahiro <0204.kahiro@gmail.com>
2019-04-20 13:28:23 +0900

    electron-packager対応

--- main.js
+++ main.js
@@ -10,11 +10,10 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({'icon': __dirname + '/icon.ico',width: 800, height: 600,})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/src/index.html`)
-
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
